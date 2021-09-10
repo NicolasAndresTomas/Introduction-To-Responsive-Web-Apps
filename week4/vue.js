9ad0a4965bdb79d36368f9df77be9558f8c43a19
NicolasAndresTomas <87789388+NicolasAndresTomas@users.noreply.github.com>
2021-09-10 17:44:07 +1000

    Add files via upload

---
+++ week4/vue.js
@@ -0,0 +1,31 @@
+var ex0 = new Vue({
+  el: '#example1-1',
+  data: {
+    message: 'Hello World!'
+  }
+})
+
+var ex1 = new Vue({
+  el: '#example1-2',
+  data: {
+    message: '...'
+  }
+})
+
+var ex2 = new Vue({
+  el: '#example2-1',
+  data: {
+    seen: true
+  }
+})
+
+var ex3 = new Vue({
+  el: '#example2-2',
+  data: {
+    todos: [
+      { text: 'element 1' },
+      { text: 'element 2' },
+      { text: 'element 3' }
+    ]
+  }
+})
